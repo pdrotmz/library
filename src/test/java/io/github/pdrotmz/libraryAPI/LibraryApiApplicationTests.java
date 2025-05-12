960ce8941f4fcea4c30e7acf3432840d57e04030
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-12 10:05:57 -0300

    first commit

---
+++ src/test/java/io/github/pdrotmz/libraryAPI/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pdrotmz.libraryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
