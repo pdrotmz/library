a488d3aaa2db0d0bb2c102abafacc0b2deb126f8
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-16 12:12:21 -0300

    feat(service): add more contracts
    - Add findAllSumaries
    - Add findBookByPriceBetween

--- src/main/java/io/github/pdrotmz/libraryAPI/service/BookService.java
+++ src/main/java/io/github/pdrotmz/libraryAPI/service/BookService.java
@@ -4,12 +4,16 @@ import io.github.pdrotmz.libraryAPI.model.Book;
 import io.github.pdrotmz.libraryAPI.projection.BookSummaryProjection;
 import io.github.pdrotmz.libraryAPI.projection.BookTitleOnly;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 public interface BookService {
     Book registerBook(Book book);
+    List<BookSummaryProjection> findAllSummaries();
     BookSummaryProjection findBookById(UUID id);
     BookTitleOnly findBookByTitle(String title);
+    List<BookSummaryProjection> findBookPriceBetween(BigDecimal initialPrice, BigDecimal finalPrice);
     void updateBookById(Book book, UUID id);
     void deleteBookById(UUID id);
 }
