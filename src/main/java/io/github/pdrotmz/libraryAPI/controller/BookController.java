bec2a9cf083f161ca8cf77da5e643b24b14ff914
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-16 16:51:14 -0300

    feat(controller): add more endpoints
    - Add findAllBooks
    - Add findBooksByCategory
    - Add findBookByIsbn

--- src/main/java/io/github/pdrotmz/libraryAPI/controller/BookController.java
+++ src/main/java/io/github/pdrotmz/libraryAPI/controller/BookController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -24,6 +26,12 @@ public class BookController {
     public ResponseEntity<Book> registerBook(@RequestBody Book book) {
         Book newBook = bookService.registerBook(book);
         return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
+    }
+
+    @GetMapping("/list-all")
+    public ResponseEntity<List<Book>> findAllBooks() {
+        List<Book> books = bookService.findAllBooks();
+        return ResponseEntity.status(HttpStatus.OK).body(books);
     }
 
     @GetMapping("/list-all/home") // TODO: Changes this endpoints for "/home"
@@ -44,12 +52,24 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(bookTitle);
     }
 
+    @GetMapping("filter-by/category/{category}")
+    public ResponseEntity<List<BookSummaryProjection>> findBooksByCategory(@PathVariable String category) {
+        List<BookSummaryProjection> booksCategory = bookService.findBooksByCategory(category);
+        return ResponseEntity.status(HttpStatus.OK).body(booksCategory);
+    }
+
     @GetMapping("/filter-by/price-between")
     public ResponseEntity<List<BookSummaryProjection>> findByBookPriceBetween(
             @RequestParam BigDecimal initialPrice,
             @RequestParam BigDecimal finalPrice) {
         List<BookSummaryProjection> bookPrices = bookService.findBookPriceBetween(initialPrice, finalPrice);
         return ResponseEntity.status(HttpStatus.OK).body(bookPrices);
+    }
+
+    @GetMapping("search-by/isbn/{isbn}")
+    public ResponseEntity<BookTitleOnly> findBookByIsbn(@PathVariable String isbn) {
+        Optional<BookTitleOnly> isbnBook = bookService.findBookByIsbn(isbn);
+        return isbnBook.map(ResponseEntity::ok).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @PutMapping("/update-by/id/{id}")
