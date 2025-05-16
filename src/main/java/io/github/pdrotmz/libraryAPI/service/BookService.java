14c0ee1d15aad0f772f023169b78913595ab71b9
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-16 16:50:24 -0300

    feat(service): add more contracts
    - Add findAllBooks
    - Add findBooksByCategory
    - Add findBookByIsbn

--- src/main/java/io/github/pdrotmz/libraryAPI/service/BookService.java
+++ src/main/java/io/github/pdrotmz/libraryAPI/service/BookService.java
@@ -6,14 +6,18 @@ import io.github.pdrotmz.libraryAPI.projection.BookTitleOnly;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BookService {
     Book registerBook(Book book);
+    List<Book> findAllBooks();
     List<BookSummaryProjection> findAllSummaries();
     BookSummaryProjection findBookById(UUID id);
     BookTitleOnly findBookByTitle(String title);
     List<BookSummaryProjection> findBookPriceBetween(BigDecimal initialPrice, BigDecimal finalPrice);
+    List<BookSummaryProjection> findBooksByCategory(String category);
+    Optional<BookTitleOnly> findBookByIsbn(String isbn);
     void updateBookById(Book book, UUID id);
     void deleteBookById(UUID id);
 }
