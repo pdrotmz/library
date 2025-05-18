ad91346b1069fd1201ac8edfd1beda6bbf516247
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-18 20:47:57 -0300

    feat(model): add another attributes
    - Adds createdAt and updatedAt
    - Adds annotations for data audit
    - Adds and comments foreign entity "author"

--- src/main/java/io/github/pdrotmz/libraryAPI/model/Book.java
+++ src/main/java/io/github/pdrotmz/libraryAPI/model/Book.java
@@ -1,9 +1,13 @@
 package io.github.pdrotmz.libraryAPI.model;
 
+import io.github.pdrotmz.libraryAPI.utils.LocalDataTimeConverter;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "books")
@@ -33,4 +37,30 @@ public class Book {
     @Column(name = "category", nullable = false, length = 50, updatable = true)
     @Enumerated(EnumType.STRING)
     private Category category;
+
+    @Column(name = "release_date", nullable = false)
+    private String releaseDate;
+
+    /* @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author; */
+
+    @Column(name = "created_at")
+    @Convert(converter = LocalDataTimeConverter.class)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @Convert(converter = LocalDataTimeConverter.class)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.setCreatedAt(LocalDateTime.now());
+        this.setUpdatedAt(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.setUpdatedAt(LocalDateTime.now());
+    }
 }
