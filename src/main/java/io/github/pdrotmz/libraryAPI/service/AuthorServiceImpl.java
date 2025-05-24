7b7d1fd0f968f07871c234f64274aee684d344d4
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-24 11:33:08 -0300

    feat(service): add implementation contract author by birth date

--- src/main/java/io/github/pdrotmz/libraryAPI/service/AuthorServiceImpl.java
+++ src/main/java/io/github/pdrotmz/libraryAPI/service/AuthorServiceImpl.java
@@ -2,6 +2,7 @@ package io.github.pdrotmz.libraryAPI.service;
 
 import io.github.pdrotmz.libraryAPI.dto.author.AuthorRequestDTO;
 import io.github.pdrotmz.libraryAPI.dto.author.AuthorResponseDTO;
+import io.github.pdrotmz.libraryAPI.exception.author.AuthorNotFoundByBirthDateException;
 import io.github.pdrotmz.libraryAPI.model.Author;
 import io.github.pdrotmz.libraryAPI.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,19 @@ public class AuthorServiceImpl implements AuthorService {
     public AuthorResponseDTO registerAuthor(AuthorRequestDTO request) {
         Author author = new Author();
         author.setName(request.name());
+        author.setBirthDate(request.birthDate());
 
         repository.save(author);
 
-        return new AuthorResponseDTO(author.getId(), author.getName(), author.getBooks());
+        return new AuthorResponseDTO(author.getId(), author.getName(), author.getBirthDate(), author.getBooks());
+    }
+
+    @Override
+    public List<Author> findAuthorsByBirthDate(int birthDate) {
+        if(repository.findAuthorsByBirthDate(birthDate).isEmpty()) {
+            throw new AuthorNotFoundByBirthDateException(birthDate);
+        }
+        return repository.findAuthorsByBirthDate(birthDate);
     }
 
     @Override
