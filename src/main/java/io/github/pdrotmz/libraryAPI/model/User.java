5907fbad47c69c0d2beab1f23b70d9dd69cbee89
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-26 01:19:28 -0300

    feat(model): add user model
    - Add User model
    - Add UserRole

---
+++ src/main/java/io/github/pdrotmz/libraryAPI/model/User.java
@@ -0,0 +1,70 @@
+package io.github.pdrotmz.libraryAPI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table(name = "tb_users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_" + this.role.name());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/io/github/pdrotmz/libraryAPI/model/UserRole.java
@@ -0,0 +1,6 @@
+package io.github.pdrotmz.libraryAPI.model;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
