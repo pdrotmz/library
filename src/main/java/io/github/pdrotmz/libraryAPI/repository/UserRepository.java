c6d1a511b76e7c3b7bfd28f19841f266843a409b
pdrotmz <pedrotomaz0210@gmail.com>
2025-05-26 01:20:09 -0300

    feat(repository): add user repository
    - Add findByEmail
    - Add findUserByUsername

---
+++ src/main/java/io/github/pdrotmz/libraryAPI/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package io.github.pdrotmz.libraryAPI.repository;
+
+import io.github.pdrotmz.libraryAPI.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
